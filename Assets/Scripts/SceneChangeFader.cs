d09871dea6120c9bdbc1dbcdd67d38797e3eeb67
Eamon <eamonoldridge@hotmail.com>
2021-11-27 03:14:48 -0800

    Added Scene fader transitions

---
+++ Assets/Scripts/SceneChangeFader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeFader : MonoBehaviour
+{
+    public Animator animator;
+    private string sceneNameToLoad;
+
+    public void FadeToScene(string sceneName)
+    {
+        sceneNameToLoad = sceneName;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(sceneNameToLoad);
+    }
+}
