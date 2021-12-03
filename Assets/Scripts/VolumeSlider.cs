47041b0111e8ac5875bca5806e12a2865a84afb2
Eamon <eamonoldridge@hotmail.com>
2021-12-02 18:14:31 -0800

    Volume Slider persists between game sessions. Tweaked menu lighting

---
+++ Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    Slider mySlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySlider = this.gameObject.GetComponent<Slider>();
+        mySlider.value = PlayerPrefs.GetFloat("masterVolume");
+    }
+
+    public void UpdateVolumeSettings()
+    {
+        PlayerPrefs.SetFloat("masterVolume", mySlider.value);
+    }
+}
